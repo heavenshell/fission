674ae7d38c905244abb8f6f66e78f5990eefa547
s_ohyanagi <heavenshell.jp@gmail.com>
2010-01-01 13:11:45 +0900

    Upgrade copyright year

--- github.py
+++ github.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf_8 -*-
+# -*- coding: utf-8 -*-
 #
 # Download arhive from Github
 #
-# Copyright (c) 2009 Shinya Ohyanagi, All rights reserved.
+# Copyright (c) 2009-2010 Shinya Ohyanagi, All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
